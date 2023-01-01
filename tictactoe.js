54c35a7c7703f9d8c856cb46881d2a1f06219c5d
nikhilpadiyath <nikhilpadiyath@gmail.com>
2023-01-01 13:59:46 +0530

    Game board and player details assigned

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,19 @@
 let gameBoardModule= (function(){
     let gameBoard = [];
-    return {};
+    let playerArray = [];
+    let makePlayerMove = () => {
+        if(gameBoard.length == 0){
+            alert("Player 1, make your move!");
+            gameBoard.push(playerArray[2]);
+        } else if(gameBoard[gameBoard.length-1]== "X"){
+            alert("Player 2, make your move!");
+            gameBoard.push(playerArray[5]);
+        } else if(gameBoard[gameBoard.length-1]=="O"){
+            alert("Player 1, make your move!");
+            gameBoard.push(playerArray[2]);
+        }
+    };
+    return {gameBoard,playerArray,makePlayerMove};
 })();
 let displayControlModule = (function(){
    const makeMove = document.querySelectorAll(".select");
@@ -24,11 +37,36 @@ let displayControlModule = (function(){
     index++;
    })
 })();
-let createPlayer = (playerName,playerNumber,assignedXO) => {
-    let getPlayerName = () => { playerName;
-        console.log("This is the name of player" + playerNumber + "...." + playerName);}
-        return{getPlayerName,playerName,playerNumber,assignedXO};
+let createPlayer = () => {
+    for(let i=0; i<4; i++){
+        if(gameBoardModule.playerArray.length>=6){
+            gameBoardModule.makePlayerMove();
+            break;
+        } else if(gameBoardModule.playerArray.length==0){
+            let playerName = prompt("What is your name?");
+            if(playerName=="" || platerName== null){
+                alert ("Sorry! Name cannot be blank");
+                continue;
+            }
+            let playerNumber=1;
+            let assignedXO="X";
+            alert ("You are player 1, and your assigned letter is X");
+            gameBoardModule.playerArray.push(playerName, playerNumber, assignedXO);
+            console.log("Show me the contents of playerArray...", gameBoardModule.playerArray);
+        } else if(gameBoardModule.playerArray.length!==0){
+        let playerName= prompt("What is your name?");
+        if(playerName=="" || platerName== null){
+            alert ("Sorry! Name cannot be blank");
+            continue;
+        }
+        let playerNumber=2;
+        let assignedXO= "O";
+        alert ("You are plater 2 and your assigned letter is O");
+        gameBoardModule.playerArray.push(playerName,playerNumber,assignedXO);
+        console.log("Show me the contents of playerArray...",gameBoardModule.playerArray);
+        }
+        }
     };
-    let Justin = createPlayer("Justin",1,"X");
-    let James = createPlayer("James",2,"O");
+
+
 
