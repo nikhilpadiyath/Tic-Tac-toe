87d3d2c627d05724dfe93b6ded2310ad310d1b40
nikhilpadiyath <nikhilpadiyath@gmail.com>
2023-07-19 20:03:35 +0530

    removed unwanted dirs

--- To-do-list/src/blank-project-load.js
+++
@@ -1,8 +0,0 @@
-export const blankProjectLoad = ()=> {
-    console.log("Called blankProjectLoad Module, creating blank project array now");
-    let projectArray = [];
-    let projectTitle = "Default project";
-    projectArray.push({projectTitle});
-    console.log(projectArray);
-    return {projectArray, projectTitle};
-}
